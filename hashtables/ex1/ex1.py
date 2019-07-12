1472967a5ae9383f7f47f88798c0e49980b7f873
Jacob McFaul <mcjakedude@gmail.com>
2019-07-12 12:03:57 -0700

    jacob-mcfaul

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,9 +1,5 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
-from hashtables import (HashTable,
-                        hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+from hashtables import (HashTable, hash_table_insert, hash_table_retrieve)
 
 
 def get_indices_of_item_weights(weights, length, limit):
@@ -12,8 +8,17 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    for i in range(length):
+        hash_table_insert(ht, weights[i], i)
+
+    for i in range(length):
+        weight = limit - weights[i]
+        otherWeight = hash_table_retrieve(ht, weight)
+        if otherWeight:
+            return (otherWeight, i)
 
     return None
+
 
 
 def print_answer(answer):
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -12,18 +12,18 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
     
-    # add all tickets with the starting location at the first index
+    # add all tickets
     for i in range(length):
         if route[i] == None:
             hash_table_insert(hashtable, tickets[i].source, tickets[i].destination)
-            if tickets[i].source == "NONE": 
-                current = tickets[i].destination
-                route[0] = current
-        
+            
     # rearrange all tickets not including the first in the right order
     for j in range(1, length):
-        if current == tickets[j].source:
-            route[j] = tickets[j].destination
-            current = route[j]
+        if tickets[i].source == "NONE": 
+                current = tickets[i].destination
+                route[0] = current
+        next_ticket = hash_table_retrieve(hashtable, current)
+        route[j], current = next_ticket
+        
 
     return route
